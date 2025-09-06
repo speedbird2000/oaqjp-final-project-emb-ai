d076f27bcc4ff5dc99079bd2a85264356cabcdf3
speedbird2000 <hansbos@inter.nl.net>
2025-09-06 03:13:18 -0400

    Initial Commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,6 +1,5 @@
 ''' Import the nescessary libraries for the emotion detector app '''
-import requests
-import json
+import requests , json
 
 def emotion_detector(text_to_analyse):
     ''' this function is to detect the emotinon in a given text '''    
@@ -12,6 +11,27 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     # Sending a POST request to the sentiment analysis API
     response = requests.post(url, json=myobj, headers=header)
-    # Parsing the JSON response from the API
-    formatted_response = response.json()
-    return formatted_response
+    # Parsing the JSON response from the API 
+    data = response.json() 
+    # get the list 
+    preds = data.get('emotionPredictions') 
+    # first item from the list 
+    emotions = preds[0].get('emotion', {}) 
+    # following are the 'emotion' items
+    anger = emotions.get('anger')
+    disgust = emotions.get('disgust') 
+    fear = emotions.get('fear') 
+    joy = emotions.get('joy') 
+    sadness = emotions.get('sadness') 
+    # determine dominant emotion
+    best_label = None
+    best_score = -1.0
+    # fixed order along the known keys
+    for key in ['joy', 'anger', 'disgust', 'fear', 'sadness']:
+        value = emotions.get(key)
+        if isinstance(value, (int, float)) and value > best_score:
+            best_label = key
+            best_score = value
+    # return dominant emotion and the other emotions
+    return {'dominant': {'label': best_label, 'score': best_score}, 'emotions': emotions}
+
